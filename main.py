fc96fcd2381fff5d61b3b0f640f2c93dec110784
Vasyl <vasyl@example.com>
2025-04-17 21:50:50 +0300

    strings practice

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(id("blabla"))
+# print(id("blabla"))
+# print(id("blabla"))
+
+
+some_string = "blabla2 \n ☂  \t їїї \U00000457 \U00002602"
+print(some_string)
+pos_number_of_a = ord("a")
+pos_number_of_umbrella = ord("\U00002602")
+char_97 = chr(97)
+valid_symbol = ""
+print(char_97)
+formatted_string = f"{char_97=} but {pos_number_of_a=}"
+pass
+print(formatted_string)
---
+++ templates.py
@@ -0,0 +1,4 @@
+SMS_TEMPLATE = "Шановний {name}, ми Вас очікуємо на послугу \"{service}\" за адресою '{address}'{smile}"
+SMS_WARNING_MSG = "{name}, Ваш аккаунт зараз зламають!!!"
+
+MSG_ENTER_NAME = "Введіть Ваше імя: "
