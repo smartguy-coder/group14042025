c84cbb3b372527468625f78b17cabe4b5cdd6707
Vasyl <vasyl@example.com>
2025-04-24 23:17:01 +0300

    bool and list

--- shop.py
+++ shop.py
@@ -4,7 +4,9 @@ from pywebio.input import slider, FLOAT, NUMBER
 
 from pywebio.input import input as pw_input
 
-from pywebio.output import put_html, put_success
+from pywebio.output import put_html, put_success, put_error
+
+import shop_clients
 
 logging.basicConfig(
     level=logging.INFO,
@@ -12,36 +14,46 @@ logging.basicConfig(
     handlers=[logging.FileHandler("shop.log"), logging.StreamHandler()],
 )
 
+# PRICES
 CHEESE_PRICE = decimal.Decimal(286.3)
 POTATO_PRICE = decimal.Decimal(40)
 
-logging.debug("debug")
-logging.info("info")
 # HEADER
 put_html("<h1>Welcome to our shop</h1>")
 
-# INPUT SECTION
+user_id = pw_input("Enter your ID", required=True)
+user_id = user_id.strip()
 
-cheese_weight = slider(
-    "Cheese", type=FLOAT, min_value=0, max_value=5, value=0.15, required=True
-)
-cheese_weight = decimal.Decimal(cheese_weight).quantize(
-    decimal.Decimal("0.000"), rounding=decimal.ROUND_HALF_UP
-)
+if user_id in shop_clients.CLIENTS or shop_clients.OPEN_DOOR_DAY:
+    cheese_weight = slider(
+        "Cheese", type=FLOAT, min_value=0, max_value=5, value=0.15, required=True
+    )
+    cheese_weight = decimal.Decimal(cheese_weight).quantize(
+        decimal.Decimal("0.000"), rounding=decimal.ROUND_HALF_UP
+    )
 
-potato_weight = pw_input("Potato", type=NUMBER, required=True, min=0, max=10, value=3)
-potato_weight = decimal.Decimal(potato_weight).quantize(
-    decimal.Decimal("0.000"), rounding=decimal.ROUND_HALF_UP
-)
-logging.info(f"{potato_weight=}")
+    potato_weight = pw_input(
+        "Potato", type=NUMBER, required=True, min=0, max=10, value=3
+    )
+    potato_weight = decimal.Decimal(potato_weight).quantize(
+        decimal.Decimal("0.000"), rounding=decimal.ROUND_HALF_UP
+    )
+    logging.info(f"{potato_weight=}")
 
-cheese_cost = (CHEESE_PRICE * cheese_weight).quantize(
-    decimal.Decimal("0.00"), rounding=decimal.ROUND_HALF_UP
-)
-potato_cost = (POTATO_PRICE * potato_weight).quantize(
-    decimal.Decimal("0.00"), rounding=decimal.ROUND_HALF_UP
-)
-total_cost = cheese_cost + potato_cost
-put_success(
-    f"Total cost: \ncheese_cost\t{cheese_cost} \npotato_cost\t{potato_cost} \ntotal_cost\t{total_cost}"
-)
+    cheese_cost = (CHEESE_PRICE * cheese_weight).quantize(
+        decimal.Decimal("0.00"), rounding=decimal.ROUND_HALF_UP
+    )
+    potato_cost = (POTATO_PRICE * potato_weight).quantize(
+        decimal.Decimal("0.00"), rounding=decimal.ROUND_HALF_UP
+    )
+    total_cost = cheese_cost + potato_cost
+    put_success(
+        f"Total cost: \ncheese_cost\t{cheese_cost} \npotato_cost\t{potato_cost} \ntotal_cost\t{total_cost}"
+    )
+else:
+    put_error("Wrong ID. Please register")
+    new_client_id = pw_input("Enter your ID").strip()
+    shop_clients.CLIENTS.append(new_client_id)
+    # put_success(str(shop_clients.CLIENTS))
+    registered_users = ",\n".join(shop_clients.CLIENTS)
+    put_success(registered_users)
---
+++ shop_clients.py
@@ -0,0 +1,24 @@
+CLIENTS = [
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhkj878%^%^",
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhfgdskj878%^%^",
+    "5656",
+    "dilugcdfgi5656",
+    "5656665",
+    "hjhkgdfghkj878%^%^",
+    "5656",
+    "22ff",
+    "5656665",
+    "hjhkhkj878%^%^",
+    "5656",
+    "dilugi5656",
+    "5656665",
+    "hjhkhkj878%^%^",
+]
+
+OPEN_DOOR_DAY = False
