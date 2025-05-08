198dd057e0cb50d05ac691ed07a49e57695d349b
Vasyl <vasyl@example.com>
2025-05-08 21:10:52 +0300

    sets and functions

---
+++ main.py
@@ -0,0 +1,24 @@
+from utils import (
+    get_unique_values,
+    get_division,
+    validate_not_hashable,
+    send_email_to_manager,
+    is_valid_email,
+    send_email,
+)
+
+my_unique_values1 = get_unique_values("some_iterable")
+my_unique_values2 = get_unique_values([])
+my_unique_values3 = get_unique_values({5, 6})
+
+
+validate_not_hashable(5)
+send_email_to_manager()
+
+email = "example@ukr.net"
+if is_valid_email(email):
+    send_email(email_body="hello", recipient=email)
+
+
+fraction = get_division(10, 0)
+print(fraction)
